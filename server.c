62de47251bfdc892d84a564572876ee250393884
Mario <57414832+MaRio0607@users.noreply.github.com>
2022-10-28 14:48:16 -0500

    Linux

--- server.c
+++ server.c
@@ -1,84 +1,102 @@
-/*
-	Live Server on port 8888
-*/
-//Use linker flag  -lws2_32
-#include<stdio.h>
-#include<io.h>
-#include<windows.h>
-#include<winsock2.h>
-// Linux will use <sockets.h>
-// #include<sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <netdb.h> // for getnameinfo()
 
-#pragma comment(lib,"ws2_32.lib") //Winsock Library
+// Usual socket headers
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
-int main(int argc , char *argv[])
+#include <arpa/inet.h>
+
+#define SIZE 1024
+#define BACKLOG 10  // Passed to listen()
+
+void report(struct sockaddr_in *serverAddress);
+
+void setHttpHeader(char httpHeader[])
 {
-	WSADATA wsa;
-	SOCKET s , new_socket;
-	struct sockaddr_in server , client;
-	int c;
-	char *message;
+    // File object to return
+    FILE *htmlData = fopen("index.html", "r");
 
-    // Initializes the library
-	if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
-	{
-		printf("Failed. Error Code : %d",WSAGetLastError());
-		return 1;
-	}
-	
-	printf("Library initialized\n");
-	
-	//Create a socket
-    // Type stream tcp
-	if((s = socket(AF_INET , SOCK_STREAM , 0 )) == INVALID_SOCKET)
-	{
-		printf("Error creating socket : %d" , WSAGetLastError());
-	}
+    char line[100];
+    char responseData[8000];
+    while (fgets(line, 100, htmlData) != 0) {
+        strcat(responseData, line);
+    }
+    // char httpHeader[8000] = "HTTP/1.1 200 OK\r\n\n";
+    strcat(httpHeader, responseData);
+}
 
-	printf("Socket created.\n");
-	
-	//Prepare the sockaddr_in structure
-	server.sin_family = AF_INET;
-	server.sin_addr.s_addr = INADDR_ANY;
-	server.sin_port = htons( 8080 );
-	
-	//Bind
-	if( bind(s ,(struct sockaddr *)&server , sizeof(server)) == SOCKET_ERROR)
-	{
-		printf("Bind error : %d" , WSAGetLastError());
-		exit(EXIT_FAILURE);
-	}
-	
-	puts("Bound");
+int main(void)
+{
+    char httpHeader[8000] = "HTTP/1.1 200 OK\r\n\n";
 
-	listen(s , 3);
-	
-	puts("Waiting for connections...");
-	
-	c = sizeof(struct sockaddr_in);
-	
-	while( (new_socket = accept(s , (struct sockaddr *)&client, &c)) != INVALID_SOCKET )
-	{
-		puts("Connection accepted");
-        //recv(new_socket, message, strlen(message), 0);
-        //puts(message);
-		//Reply to the client
-		message = "Hello socket\n";
-		send(new_socket , message , strlen(message) , 0);
-		write(new_socket, "HTTP/1.1 200 OK\n", 16);
-		write(new_socket, "Content-length: 46\n", 19);
-		write(new_socket, "Content-Type: text/html\n\n", 25);
-		write(new_socket, "<html><body><H1>Hello world</H1></body></html>",46);
-	}
-	
-	if (new_socket == INVALID_SOCKET)
-	{
-		printf("accept failed with error code : %d" , WSAGetLastError());
-		return 1;
-	}
+    // Socket setup: creates an endpoint for communication, returns a descriptor
+    // -----------------------------------------------------------------------------------------------------------------
+    int serverSocket = socket(
+        AF_INET,      // Domain: specifies protocol family
+        SOCK_STREAM,  // Type: specifies communication semantics
+        0             // Protocol: 0 because there is a single protocol for the specified family
+    );
 
-	closesocket(s);
-	WSACleanup();
-	
-	return 0;
+    // Construct local address structure
+    // -----------------------------------------------------------------------------------------------------------------
+    struct sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(8001);
+    serverAddress.sin_addr.s_addr = htonl(INADDR_LOOPBACK);//inet_addr("127.0.0.1");
+
+    // Bind socket to local address
+    // -----------------------------------------------------------------------------------------------------------------
+    // bind() assigns the address specified by serverAddress to the socket
+    // referred to by the file descriptor serverSocket.
+    bind(
+        serverSocket,                         // file descriptor referring to a socket
+        (struct sockaddr *) &serverAddress,   // Address to be assigned to the socket
+        sizeof(serverAddress)                 // Size (bytes) of the address structure
+    );
+
+    // Mark socket to listen for incoming connections
+    // -----------------------------------------------------------------------------------------------------------------
+    int listening = listen(serverSocket, BACKLOG);
+    if (listening < 0) {
+        printf("Error: The server is not listening.\n");
+        return 1;
+    }
+    report(&serverAddress);     // Custom report function
+    setHttpHeader(httpHeader);  // Custom function to set header
+    int clientSocket;
+
+    // Wait for a connection, create a connected socket if a connection is pending
+    // -----------------------------------------------------------------------------------------------------------------
+    while(1) {
+        clientSocket = accept(serverSocket, NULL, NULL);
+        send(clientSocket, httpHeader, sizeof(httpHeader), 0);
+        close(clientSocket);
+    }
+    return 0;
+}
+
+void report(struct sockaddr_in *serverAddress)
+{
+    char hostBuffer[INET6_ADDRSTRLEN];
+    char serviceBuffer[NI_MAXSERV]; // defined in `<netdb.h>`
+    socklen_t addr_len = sizeof(*serverAddress);
+    int err = getnameinfo(
+        (struct sockaddr *) serverAddress,
+        addr_len,
+        hostBuffer,
+        sizeof(hostBuffer),
+        serviceBuffer,
+        sizeof(serviceBuffer),
+        NI_NUMERICHOST
+    );
+    if (err != 0) {
+        printf("It's not working!!\n");
+    }
+    printf("\n\n\tServer listening on http://%s:%s\n", hostBuffer, serviceBuffer);
 }
